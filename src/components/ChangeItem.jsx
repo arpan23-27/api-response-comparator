83488bdfc54e9a5a2799fc1587996f81ba12c7b8
Arpan Shukla <devarpan23@gmail.com>
2026-08-25 21:47:00 +0530

    Show old and new values for changes

--- src/components/ChangeItem.jsx
+++ src/components/ChangeItem.jsx
@@ -2,6 +2,22 @@ function ChangeItem({ change }) {
   return (
     <div>
       <strong>{change.path}</strong>: {change.type}
+    {change.type  === 'modified' && (
+        <div>
+            <p>Old: {JSON.stringify(change.oldValue)}</p>
+            <p>New: {JSON.stringify(change.newValue)}</p>
+        </div>
+    )}
+
+    {change.type ===  'added' && (
+        <p>New: {JSON.stringify(change.newValue)}</p>
+    )}
+
+    
+    {change.type === 'removed' && (
+        <p>Old: {JSON.stringify(change.oldValue)}</p>
+      )}
+
     </div>
   );
 }
